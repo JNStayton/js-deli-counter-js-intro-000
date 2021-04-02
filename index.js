0d78919932a2876bc2da45ee4122e67f3257683e
Jessica Stayton <jessica.stayton@gmail.com>
2021-04-02 22:08:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (katzDeliLine, name) {
       katzDeliLine.push(name);
-      return `Welcome ${name}, 
+      return `Welcome ${name},
       you are number ${katzDeliLine.length} in line.`
 }
