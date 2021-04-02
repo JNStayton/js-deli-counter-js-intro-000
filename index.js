0761d0729c23ee3167ba632bfaf43d4700e62d5d
Jessica Stayton <jessica.stayton@gmail.com>
2021-04-02 22:25:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ function nowServing (katzDeliLine) {
           return `There is nobody waiting to be served!`;
         }
 }
+
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+      return `The line is currently empty.`
+  }
+}
