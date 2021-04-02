b6564d7b6918e7d49ae5d424ef2a798ce80ebfe2
Jessica Stayton <jessica.stayton@gmail.com>
2021-04-02 22:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function nowServing (katzDeliLine) {
 
 
 function currentLine (katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-      return `The line is currently empty.`
-  }
-}
+    var waiting = [];
+        if (katzDeliLine.length === 0) {
+          return `The line is currently empty.`;
+        }
+        for (var i = 0; i < katzDeliLine.length; i++) {
+          waiting.push(` ${i+1}, ${katzDeliLine[i]}`);
+        }
+    return "The line is currently:" + waiting;
